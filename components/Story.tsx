94b01d2c2bc118dd6ac18035b184627f9f5d5737
Jastin123423 <Jjovinatha@gmail.com>
2025-12-21 10:53:31 +0300

    Update Story.tsx

--- components/Story.tsx
+++ components/Story.tsx
@@ -1,5 +1,6 @@
+
 import React, { useState, useEffect, useRef } from 'react';
-import { Story, User, Song } from '../types';
+import { Story, User, Song, Reaction } from '../types';
 import { MOCK_SONGS, INITIAL_USERS } from '../constants';
 
 interface StoryViewerProps {
